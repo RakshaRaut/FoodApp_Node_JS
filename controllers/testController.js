da6d0cfed8d5b0b2b0b15cd2645296e2aa7b7a06
Raksha Raut <raksharaut174@gmail.com>
2025-01-11 15:19:39 +0545

    add login functionality

--- controllers/testController.js
+++ controllers/testController.js
@@ -1,3 +1,5 @@
+const userModel = require("../models/userModel");
+
 const testUserController = (req, res) => {
     try{
         res.status(200).send("<h1>Welcome to Hamro Bhansa </h1>");
@@ -6,4 +8,38 @@ const testUserController = (req, res) => {
     }
 };
 
-module.exports = { testUserController };
+// LOGIN
+const loginController = async (req,res) => {
+    try{
+        const {email,password} = req.body
+        // validification
+        if(!email || !password){
+         return res.status(500).send({
+            success: false,
+            message:'Please provide email or password'
+         })   
+        }
+        // check user
+        const user = await userModel.findOne({email: email, password: password})
+        if(!user){
+            return res.status(404).send({
+                success:false,
+                message: 'User Not Found OR Password MisMatch'
+            })
+        }
+        res.status(200).send({
+            success: true,
+            message: 'Login Successfully',
+            user,
+        })
+    } catch(error) {
+        console.log(error)
+        res.status(500).send({
+            success: false,
+            message:'Error In Login API',message: 'Error In Login API',
+            error
+        })
+    }
+};
+
+module.exports = { testUserController, loginController };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const { registerController } = require('../controllers/authControllers');
+const { loginController } = require('../controllers/testController');
 
 const router = express.Router();
 
 // routes
 // REGISTER || POST
 router.post('/register', registerController);
+
+// LOGIN || POST
+router.post('/login', loginController);
 
 module.exports = router;
